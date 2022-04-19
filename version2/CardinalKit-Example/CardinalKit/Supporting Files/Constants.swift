466cd7e2c2817441193ca9a67fea918d56fa342a
Esteban Ramos <julianramos@alternova.co>
2022-04-19 11:18:19 -0500

    Program local notifications every day at 7 pm

--- version2/CardinalKit-Example/CardinalKit/Supporting Files/Constants.swift
+++ version2/CardinalKit-Example/CardinalKit/Supporting Files/Constants.swift
@@ -13,6 +13,7 @@ class Constants {
     static let prefConfirmedLogin = "PREF_CONFIRMED_LOGIN"
     static let prefFirstRunWasMarked = "PREF_FIRST_RUN"
     static let prefUserEmail = "PREF_USER_EMAIL"
+    static let prefsNotificationsSchedule = "PREFS_NOTIFICATIONS_SCHEDULE"
     
     static let prefCareKitCoreDataInitDate = "PREF_CORE_DATA_INIT_DATE"
     static let prefHealthRecordsLastUploaded = "PREF_HEALTH_LAST_UPLOAD"
