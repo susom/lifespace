4cf8d81cccb3bd58a607881fe13e089085f13cb2
Vishnu Ravi <vishnu.ravi@gmail.com>
2022-06-14 18:31:16 -0400

    Fixes consent verification for existing user onboarding workflow (#1)
    * Fixes consent check for existing user login workflow
    
    * Clean up

--- version2/LifeSpace/CardinalKit/Components/Onboarding/CKReviewConsentDocument.swift
+++ version2/LifeSpace/CardinalKit/Components/Onboarding/CKReviewConsentDocument.swift
@@ -11,45 +11,53 @@ import Firebase
 import ResearchKit
 
 
-public class CKReviewConsentDocument: ORKQuestionStep{
+public class CKReviewConsentDocument: ORKQuestionStep {
+    
+    // Boolean step that checks for the existence of a consent document
+    // and sets its answer to the result. Used for navigation during
+    // onboarding.
+    
     public override init(
         identifier: String
     ) {
         super.init(identifier: identifier)
         self.answerFormat = ORKAnswerFormat.booleanAnswerFormat()
     }
-
+    
     @available(*, unavailable)
     public required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
 
-public class CKReviewConsentDocumentViewController:ORKQuestionStepViewController{
-    public var CKReviewConsentDocument: CKReviewConsentDocument!{
+public class CKReviewConsentDocumentViewController:ORKQuestionStepViewController {
+    public var CKReviewConsentDocument: CKReviewConsentDocument! {
         return step as? CKReviewConsentDocument
     }
     
     public override func viewDidLoad() {
         let storage = Storage.storage()
         let storageRef = storage.reference()
-        // REVIEW IF DOCUMENT EXIST
+        
+        // Attempt to download consent document from cloud storage
         if let DocumentCollection = CKStudyUser.shared.consentCollection {
             let config = CKPropertyReader(file: "CKConfiguration")
             let consentFileName = config.read(query: "Consent File Name")
             let DocumentRef = storageRef.child("\(DocumentCollection)\(consentFileName).pdf")
-            // Create local filesystem URL
+            
             var docURL = (FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)).last as NSURL?
             docURL = docURL?.appendingPathComponent("\(consentFileName).pdf") as NSURL?
             let url = docURL! as URL
-            // Download to the local filesystem
-            let downloadTask = DocumentRef.write(toFile: url) { url, error in
-              if let error = error {
-                  print(error.localizedDescription)
-                  self.setAnswer(false)
-              } else {
-                  self.setAnswer(true)
-              }
+            
+            DocumentRef.write(toFile: url) { url, error in
+                if let error = error {
+                    // Couldn't download consent document
+                    print("Error downloading consent document: " + error.localizedDescription)
+                    self.setAnswer(false)
+                } else {
+                    // Successfully downloaded consent document
+                    self.setAnswer(true)
+                }
                 super.goForward()
             }
         }
